4dde669d5893c8dc5454f6b3b2ef44ca03ba5cd7
Incendie <incendie323@gmail.com>
2017-10-05 12:37:43 -0400

    basic functionality working

--- dev/scripts/Lorem.js
+++ dev/scripts/Lorem.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import phrases from './phrases';
 
-const Lorem = () => {
+const Lorem = (props) => {
+  let sentences = "";
+
+  for (let i = 0; i < props.numSentences; i++) {
+    let randomPick = Math.floor(Math.random() * phrases.length);
+    sentences += phrases[randomPick] + " ";
+  };
+
   return (
     <div>
-      <p>Lorem Ipsum</p>
+      <p>{sentences}</p>
     </div>
   );
 };
---
+++ dev/scripts/phrases.js
@@ -0,0 +1,21 @@
+const phrases = [
+  "Never limit yourself.",
+  "I just want to eat spaghetti until my heart fails.",
+  "He is the git push origin master.",
+  "DUGGIE.",
+  "color: blue.",
+  "And let me tell you, if you believe, you will achieve, anything you set your mind to, never limit yourself.",
+  "I think this could use a hover state.",
+  "Just give it a hard refresh.",
+  "But did you save it?",
+  "*Fatin spins*",
+  "*Fatin breaks out into salsa*",
+  "ok.",
+  "Do me a favour and have yourself a great weekend.",
+  "Neato burrito.",
+  "Overall this project wasn't so hard oh but THIS ANIMATION here really gave me a lot of trouble and was really annoying to figure out.",
+  "HENH HENH.",
+  "Putt Putt Goes to the Moon.",
+]
+
+export default phrases;
