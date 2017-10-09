d348d055b993e4a96cb319ff1008b6b60d44b6a2
Incendie <incendie323@gmail.com>
2017-10-09 01:08:11 -0400

    some responsiveness, animations

--- dev/scripts/Lorem.js
+++ dev/scripts/Lorem.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import phrases from './phrases';
+import React from "react";
+import phrases from "./phrases";
 
-const Lorem = (props) => {
+const Lorem = props => {
   let sentence = "";
   let paragraph = [];
 
-
   for (let i = 0; i < props.numSentences; i++) {
     // for (let _i = 0; _i < 5; _i++) {
-      // let randomPick = Math.floor(Math.random() * phrases.length);
-      // sentences += phrases[randomPick] + " ";
-      // sentences = sentences.charAt(0).toUpperCase() + sentences.slice(1);
-      phrases.sort(function() {
-        return 0.1 - Math.random();
-      });
-      sentence = phrases.slice(0, 5).join(' ') + ". ";
-      sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1);
-      paragraph.push(sentence)
+    // let randomPick = Math.floor(Math.random() * phrases.length);
+    // sentences += phrases[randomPick] + " ";
+    // sentences = sentences.charAt(0).toUpperCase() + sentences.slice(1);
+    phrases.sort(function() {
+      return 0.1 - Math.random();
+    });
+    sentence = phrases.slice(0, 5).join(" ") + ". ";
+    sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1);
+    paragraph.push(sentence);
     // };
-  };
-
-
+  }
 
   return (
     <div className="animated fadeInUp">
